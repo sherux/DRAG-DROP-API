967e83abbd13f25ae5fb4daf7b6c2b3f6addd898
abbas ali <abbassheru007@gmail.com>
2023-03-21 17:55:26 +0530

    add

--- dist/util/db.js
+++ dist/util/db.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
 require("dotenv").config();
-exports.sequelize = new sequelize_1.Sequelize("DRAG&DROP", "abbasali", "Abbas@123", {
+exports.sequelize = new sequelize_1.Sequelize("DRAG&DROP", "", "", {
     dialect: "mysql",
     host: "localhost",
 });
