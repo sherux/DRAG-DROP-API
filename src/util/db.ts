29cf8f57f8917798042288f10055d08bf6392c69
abbas ali <abbassheru007@gmail.com>
2023-03-21 18:00:51 +0530

    add

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 require("dotenv").config();
-export const sequelize = new Sequelize("DRAG&DROP", "", "", {
+export const sequelize = new Sequelize("DRAG&DROP", "abbasali", "Abbas@123", {
   dialect: "mysql",
   host: "localhost",
 });
